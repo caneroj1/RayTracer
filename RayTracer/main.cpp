8f769c51a0918d3c35f70f6e179c0e9cdde44a47
caneroj1 <caneroj1@tcnj.edu>
2016-01-28 20:08:53 -0500

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Joseph Canero on 1/28/16.
+//  Copyright © 2016 jcanero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
